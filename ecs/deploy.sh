1243ec061a06231a46f5ae4c83b4d4e36fc8e542
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2021-04-03 11:33:37 +0530

    Update deploy.sh

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
-
+set -x
+#Constants
+PATH=$PATH:/usr/local/bin; export PATH
 REGION=us-west-1
-SERVICE_NAME=aws-service-latest
-CLUSTER=aws-ecs-cluster
-IMAGE_VERSION=latest
-TASK_FAMILY="aws-task-latest"
-
-# Create a new task definition for this build
+REPOSITORY_NAME=springboot-ecr
+CLUSTER=springboot-cluster
+FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' aws-task-latest.json`
+NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' aws-task-latest.json`
+SERVICE_NAME=${NAME}-service
+env
 aws configure list
+echo $HOME
 
-aws ecs register-task-definition --family aws-task-latest --cli-input-json file://ecs/aws-task-latest.json
+#Store the repositoryUri as a variable
+REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NAME} --region ${REGION} | jq .repositories[].repositoryUri | tr -d '"'`
 
-# Update the service with the new task definition and desired count
-REVISION=`aws ecs describe-task-definition --region ${REGION} --task-definition aws-task-latest | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//' `
+#Replace the build number and respository URI placeholders with the constants above
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" aws-task-latest.json > ${NAME}-v_${BUILD_NUMBER}.json
+
+#Register the task definition in the repository
+aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 
+#Get latest revision
+REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION} | jq .taskDefinition.revision`
 
 #Create or update service
-if [[ "$SERVICES" == "" ]]; then
+if [ "$SERVICES" == "" ]; then
   echo "entered existing service"
   DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
-  if [[ ${DESIRED_COUNT} = "0" ]]; then
+  if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
   fi
-  aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT} --deployment-configuration maximumPercent=100,minimumHealthyPercent=0
+  aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
 else
   echo "entered new service"
-  aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${TASK_FAMILY} --cluster ${CLUSTER} --region ${REGION}
+  aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --region ${REGION}
 fi
